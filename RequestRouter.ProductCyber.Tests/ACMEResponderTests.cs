c81ce3484826584332fe4d3c37c032eda8b47081
Robert Williams <robert.williams@lloyds.com>
2020-03-18 15:48:07 +0000

    #3 Added ACMEResponder

--- RequestRouter.ProductCyber.Tests/ACMEResponderTests.cs
+++ RequestRouter.ProductCyber.Tests/ACMEResponderTests.cs
@@ -1,8 +1,9 @@
 ﻿namespace RequestRouter.ProductCyber.Tests
 {
+    using RequestRouter;
     using RequestRouter.ProductCyber.Responders;
     using Xunit;
-    public class ACMEResponderTests
+    public class ACMEResponderTests: ACMEResponder
     {
         [Fact]
         public void ResponderExistsAndIsResponderBase()
@@ -18,7 +19,17 @@
             Assert.IsAssignableFrom<ResponseBase>(response);
         }
 
+        [Fact]
+        public void ResponderGetResponseReturnsAStandardResponse()
+        {
+            var standardRequest = new StandardRequest();
+            var response = GetResponse(standardRequest);
+            Assert.IsAssignableFrom<StandardResponseBase>(response);
+        }
 
-
+        protected override StandardResponseBase GetResponse(StandardRequestBase standardRequest)
+        {
+            return base.GetResponse(standardRequest);
+        }
     }
 }
--- RequestRouter.ProductCyber/Responders/ACMEResponder.cs
+++ RequestRouter.ProductCyber/Responders/ACMEResponder.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using RequestRouter.ProductCyber.Responders;
+using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace RequestRouter.ProductCyber
@@ -8,7 +10,21 @@ namespace RequestRouter.ProductCyber
     {
         protected override StandardResponseBase GetResponse(StandardRequestBase standardRequest)
         {
-            throw new NotImplementedException();
+
+            var standardResponse = new StandardResponse
+            {
+                Premium = 8394.00m,
+                PremiumTRIA = 755.00m,
+                Limit = 2000000.00m,
+                SurplusLinesTax = 411.96m,
+                StampingFee = 12.74m,
+                AgencyFee = 100.00m,
+                Details = "Lasdkjgasdg ahs dash asdkjh kj",
+            };
+
+            return standardResponse;
         }
+
+        
     }
 }
